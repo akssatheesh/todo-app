92a552f09e5dc011ea7405b884d0b5c3bfae288c
akssatheesh <satheeshaks2003@gmail.com>
2025-08-10 19:03:46 +0530

    added delete task featurea

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function addTask() {
   if (taskText) {
     let li = document.createElement('li');
     li.textContent = taskText;
+
+    let btn = document.createElement('button');
+    btn.textContent = "❌";
+    btn.onclick = function() { li.remove(); };
+    li.appendChild(btn);
+
     document.getElementById('tasks').appendChild(li);
     document.getElementById('taskInput').value = '';
   }
